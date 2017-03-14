f8d036631ec2b58a0e63b4069feebea4503fc9b3
Henjo <HenrikSJ_4@hotmail.com>
2017-03-14 12:09:31 +0100

    henjosimon

---
+++ I4SWT/Test Driven Development Opg/CreditCard/CreditCard.Test.Unit/CreditCardUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CreditCard.Test.Unit
+{
+
+    [TestFixture]
+    public class CreditCardUnitTests
+    {
+        private CCard _uut;
+        private string firstName;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            _uut = new CCard(firstName);
+        }
+        [Test]
+        public void CCard_IsLatinLettersOnly()
+        {
+            firstName = "simon121";
+            Assert.That(_uut.IsLatinLetter(firstName), Is.EqualTo(firstName.Length));
+        }
+    }
+}
