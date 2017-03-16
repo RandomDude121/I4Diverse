b86b3329e4c00f8082fb6c82784b166af569e986
Kenny <201409301@post.au.dk>
2017-03-16 08:57:39 +0100

    TDD

---
+++ I4SWT/TDD_Q/UltraCreditCardValidator/UltraCreditCardValidator/ICreditCardValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraCreditCardValidator
+{
+	public interface ICreditCardValidator
+	{
+		bool ValidateName(string name);
+		bool ValidateCardNumber(string cardNumber);
+		bool ValidateExpiryDate(string expiryDate);
+		bool ValidateCVV(string cvv);
+	}
+}
